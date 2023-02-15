ae7e9219a275cbc33be19c6cc1bcbb023d37cc93
KattyBtd <83547030+KattyBtd@users.noreply.github.com>
2023-02-16 00:26:42 +0100

    Add files via upload

--- lib/mylib.dart
+++ lib/mylib.dart
@@ -16,44 +16,45 @@ import 'controller/language_contoller.dart';
 
 bool adminConnect = false;
 
-const Decoration background1 = BoxDecoration(
-  gradient: LinearGradient(
-    begin: Alignment.topLeft,
-    end: Alignment(0.8, 1),
-    colors: <Color>[
-      Color.fromARGB(255, 255, 255, 0),
-      Color.fromARGB(138, 35, 175, 74),
-      Color.fromARGB(160, 54, 192, 201),
-      Color.fromARGB(255, 41, 59, 229),
-    ],
-    tileMode: TileMode.mirror,
-  ),
-);
+
 
 const TextStyle titleStyle = TextStyle(
   fontSize: 21,
-  fontFamily: 'Spline Sans Mono',
-  fontWeight: FontWeight.bold,
-  color: Colors.white,
-  shadows: <Shadow>[
+  fontFamily: 'Nunito',
+  //fontWeight: FontWeight.bold,
+ // color: Colors.white,
+  /*shadows: <Shadow>[
     Shadow(
         offset: Offset(-2.0, 2.0),
         blurRadius: 3.0,
         color: Color.fromARGB(195, 105, 105, 105))
-  ],
+  ],*/
+  letterSpacing: -1,
+);
+const TextStyle titleStyleDuration = TextStyle(
+  fontSize: 15,
+  fontFamily: 'Nunito',
+  //fontWeight: FontWeight.bold,
+ // color: Colors.white,
+  /*shadows: <Shadow>[
+    Shadow(
+        offset: Offset(-2.0, 2.0),
+        blurRadius: 3.0,
+        color: Color.fromARGB(195, 105, 105, 105))
+  ],*/
   letterSpacing: -1,
 );
 const TextStyle titleStyle2 = TextStyle(
   fontSize: 25,
-  fontFamily: 'Spline Sans Mono',
+  fontFamily: 'Nunito',
   fontWeight: FontWeight.bold,
-  color: Colors.white,
-  shadows: <Shadow>[
+  //color: Colors.white,
+  /*shadows: <Shadow>[
     Shadow(
         offset: Offset(-2.0, 2.0),
         blurRadius: 3.0,
         color: Color.fromARGB(195, 105, 105, 105))
-  ],
+  ],*/
   letterSpacing: -1,
 );
 const TextStyle titleStyle3 = TextStyle(
@@ -74,27 +75,27 @@ const TextStyle titleStyle5 = TextStyle(
   fontSize: 20,
   fontFamily: 'Nunito',
   fontWeight: FontWeight.bold,
-  color: Colors.white,
+ // color: Colors.white,
   letterSpacing: 1,
 );
 
 const TextStyle buttonTextStyle = TextStyle(
     fontSize: 20,
     fontWeight: FontWeight.bold,
-    color: Colors.white,
+    //color: Colors.white,
     fontFamily: 'Nunito',
     letterSpacing: 1);
 
 const TextStyle blueText = TextStyle(
   fontSize: 20,
   fontWeight: FontWeight.w800,
-  color: Color.fromARGB(255, 41, 59, 229),
+  color: Color.fromARGB(255, 24, 24, 24),
   fontFamily: 'Nunito',
   letterSpacing: 1,
 );
 const TextStyle blueText1 = TextStyle(
   fontSize: 12,
-  color: Color.fromARGB(255, 41, 59, 229),
+  //color: Color.fromARGB(255, 41, 59, 229),
   fontFamily: 'Spline Sans Mono ',
   letterSpacing: 1,
   //shadows:<Shadow> [ Shadow(offset:Offset(-1.0, 1.0) , blurRadius: 5.0 ,color: Color.fromARGB(195, 105, 105, 105))],
@@ -102,7 +103,7 @@ const TextStyle blueText1 = TextStyle(
 const TextStyle blueText2 = TextStyle(
   fontSize: 15,
   fontWeight: FontWeight.w600,
-  color: Color.fromARGB(255, 41, 59, 229),
+  color: Colors.black,
   fontFamily: 'Spline Sans Mono ',
   letterSpacing: 1,
   //shadows:<Shadow> [ Shadow(offset:Offset(-1.0, 1.0) , blurRadius: 5.0 ,color: Color.fromARGB(195, 105, 105, 105))],
@@ -110,7 +111,7 @@ const TextStyle blueText2 = TextStyle(
 const TextStyle radioText = TextStyle(
   fontSize: 15,
   fontWeight: FontWeight.w600,
-  color: Color.fromARGB(255, 41, 59, 229),
+ // color: Color.fromARGB(255, 41, 59, 229),
   fontFamily: 'Nunito',
   letterSpacing: 1,
   //shadows:<Shadow> [ Shadow(offset:Offset(-1.0, 1.0) , blurRadius: 5.0 ,color: Color.fromARGB(195, 105, 105, 105))],
@@ -118,7 +119,14 @@ const TextStyle radioText = TextStyle(
 
 const TextStyle simpleText = TextStyle(
   fontSize: 15,
-  color: Color.fromARGB(255, 117, 106, 106),
+  color: Colors.white,
+  fontFamily: 'Spline Sans Mono',
+  letterSpacing: 1,
+);
+
+const TextStyle simpleTextdark = TextStyle(
+  fontSize: 15,
+  color: Colors.black,
   fontFamily: 'Spline Sans Mono',
   letterSpacing: 1,
 );
@@ -264,7 +272,7 @@ createQuitButton(BuildContext context, double width, double height, var path, Ma
     child: ElevatedButton(
       style: ElevatedButton.styleFrom(
         foregroundColor: Colors.white,
-        backgroundColor: Color.fromARGB(255, 41, 59, 229),
+        backgroundColor: Color.fromARGB(255, 13, 12, 32),
         side: const BorderSide(color: Colors.white, width: 1),
         elevation: 15,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
@@ -278,7 +286,7 @@ createQuitButton(BuildContext context, double width, double height, var path, Ma
           ),
         );
       },
-      child: Text("Quit", style: buttonTextStyle),
+      child: Text("Quit", style: titleStyle),
     ),
   );
 }
@@ -291,7 +299,7 @@ createNextButton(String text, BuildContext context, double width, double height,
     child: ElevatedButton(
       style: ElevatedButton.styleFrom(
         foregroundColor: Colors.white,
-        backgroundColor: Color.fromARGB(255, 41, 59, 229),
+        //backgroundColor: Color.fromARGB(255, 41, 59, 229),
         side: const BorderSide(color: Colors.white, width: 1),
         elevation: 15,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
@@ -317,7 +325,7 @@ createtButton(String text, BuildContext context, double width, double height) {
     child: ElevatedButton(
       style: ElevatedButton.styleFrom(
         //foregroundColor: Colors.white,
-        backgroundColor: Color.fromARGB(255, 41, 59, 229),
+        backgroundColor: Color.fromARGB(255, 13, 12, 32),
         //side: const BorderSide(color: Colors.white, width: 1),
         elevation: 15,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
@@ -325,7 +333,7 @@ createtButton(String text, BuildContext context, double width, double height) {
       onPressed: () {},
       child: Text(
         text,
-        style: buttonTextStyle,
+        style: titleStyle,
       ),
     ),
   );
@@ -338,7 +346,7 @@ createMenu(BuildContext context) {
     borderRadius: BorderRadius.circular(30),
     child: Container(
       padding: EdgeInsets.all(10),
-      width: 240,
+      width: 260,
       child: Drawer(
         backgroundColor: Color.fromARGB(255, 30, 77, 233),
         child: Column(
@@ -347,7 +355,7 @@ createMenu(BuildContext context) {
             ClipRRect(
               borderRadius: BorderRadius.circular(10),
               child: Container(
-                width: 200,
+                width: 230,
                 height: 58,
                 child: ElevatedButton(
                     style: ElevatedButton.styleFrom(
@@ -387,7 +395,7 @@ createMenu(BuildContext context) {
             ClipRRect(
               borderRadius: BorderRadius.circular(10),
               child: Container(
-                width: 200,
+                width: 230,
                 height: 58,
                 child: ElevatedButton(
                     style: ElevatedButton.styleFrom(
@@ -420,7 +428,7 @@ createMenu(BuildContext context) {
             ClipRRect(
               borderRadius: BorderRadius.circular(10),
               child: Container(
-                width: 200,
+                width: 230,
                 height: 58,
                 child: ElevatedButton(
                     style: ElevatedButton.styleFrom(
@@ -446,7 +454,7 @@ createMenu(BuildContext context) {
             ClipRRect(
               borderRadius: BorderRadius.circular(10),
               child: Container(
-                width: 200,
+                width: 230,
                 height: 58,
                 child: ElevatedButton(
                     style: ElevatedButton.styleFrom(
