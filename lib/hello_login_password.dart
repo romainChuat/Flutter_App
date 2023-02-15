8c838ac864390e7b29213390b40d2dd652da41d2
KattyBtd <83547030+KattyBtd@users.noreply.github.com>
2023-02-16 00:37:19 +0100

    Update hello_login_password.dart

--- lib/hello_login_password.dart
+++ lib/hello_login_password.dart
@@ -158,7 +158,7 @@ class _hellologinpassword extends State<hellologinpassword> {
             Container(
               height: double.infinity,
               width: double.infinity,
-              decoration: mylib.background1,
+             // decoration: mylib.background1,
               child: Center(
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
